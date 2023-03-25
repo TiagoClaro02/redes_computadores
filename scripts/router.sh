7f5ede04edde7f9cd769868d28a3d1962ce6f439
TiagoClaro02 <125928799+TiagoClaro02@users.noreply.github.com>
2023-03-25 17:57:02 +0000

    Update router.sh

--- scripts/router.sh
+++ scripts/router.sh
@@ -1,1 +1,7 @@
-
+/system reset-configuration
+y
+/ip firewall nat enable 0
+/ip address add address=172.16.1.49/24 interface=ether1
+/ip address add address=172.16.41.254/24 interface=ether2
+/ip route add dst-address=172.16.40.0/24 gateway=172.16.41.253
+/ip route add dst-address=0.0.0.0/0 gateway=172.16.2.254
